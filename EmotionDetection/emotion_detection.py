0cb79072e343687103bc11a2f10acc992923648f
GYaamini <gowriyaamini@gmail.com>
2025-01-16 05:27:46 -0500

    updated code according to PEP8 guidelines

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,32 +1,38 @@
-import requests
+''' Execution of emotion_detector method using Watson AI's
+    Emotion Predict module API
+    The detected emotions are send in a dictionary format
+'''
 import json
+import requests
 
 def emotion_detector(text_to_analyse):
-    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    Input = { "raw_document": { "text": text_to_analyse } }
+    ''' Function to execute emotion detection with the help of WatsonAI API for Emotion Prediction
+        if the input is valid it returns response status code 200 conditional block result and 
+        if the input is invalid the result is sent from response status code 400 conditional block
+    '''
 
-    response = requests.post(URL, json = Input, headers = Headers)
-    
+    domain = 'https://sn-watson-emotion.labs.skills.network/v1/'
+    path = 'watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    url = domain + path
+
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_text = { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(url, json = input_text, headers = headers, timeout = 20)
+    emotions = json.loads(response.text)["emotionPredictions"][0]["emotion"]
+
     if response.status_code == 200:
-        emotions = json.loads(response.text)["emotionPredictions"][0]["emotion"]
         dominant_emotion = max(emotions.values())
         for emotion,score in emotions.items():
             if score == dominant_emotion:
                 emotions["dominant_emotion"] = emotion
                 break
 
-        return emotions
-    
     if response.status_code == 400:
+        for emotion in emotions.keys():
+            emotions[emotion] = None
 
-        return {"anger": None, 
-                "disgust": None, 
-                "fear": None, 
-                "joy": None, 
-                "sadness": None, 
-                "dominant_emotion": None
-                }
+    return emotions
 
 if __name__ == "__main__":
     text = input("Enter the text input : ")
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' Execution of Emotion Detection using emotion_detector method and 
+    deployment using flask framework under localhost:5000
+'''
+
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,25 +9,34 @@ app = Flask("Emotion Detector")
 
 @app.route('/')
 def render_index():
+    ''' Functiion to render index.html page '''
     return render_template("index.html")
 
 @app.route('/emotionDetector')
-def emotionDetector():
+def emotion_detector_deployment():
+    ''' Function handle /emotionDetector as it receives text to analyze input from
+        the interface and the emotion_detector returns a dictionary of emotions
+        with teir respective scores. This method deploys result according to the input status
+    '''
+
     text_to_analyze = request.args.get("textToAnalyze")
 
     emotion = emotion_detector(text_to_analyze)
 
     if emotion["dominant_emotion"] is None :
         return "Invalid text! Please try again!"
-    else:
-        anger = "'anger':" + str(emotion['anger'])
-        disgust = "'disgust':" + str(emotion['disgust'])
-        fear = "'fear':" + str(emotion['fear'])
-        joy = "'joy':" + str(emotion['joy'])
-        sadness = "'sadness':" + str(emotion['sadness'])
-        formatted_response = f"For the given statement, the system response is {anger}, {disgust}, {fear}, {joy} and {sadness}. The dominant emotion is {emotion['dominant_emotion']}."
-        
-        return formatted_response
+
+    anger = "'anger':" + str(emotion['anger'])
+    disgust = "'disgust':" + str(emotion['disgust'])
+    fear = "'fear':" + str(emotion['fear'])
+    joy = "'joy':" + str(emotion['joy'])
+    sadness = "'sadness':" + str(emotion['sadness'])
+    res1 = "For the given statement, the system response is "
+    res2 = f"{anger}, {disgust}, {fear}, {joy} and {sadness}."
+    res3 = f" The dominant emotion is {emotion['dominant_emotion']}."
+    formatted_response = res1 + res2 + res3
+
+    return formatted_response
 
 if __name__ == "__main__":
     app.run(debug = True, port = 5000)
