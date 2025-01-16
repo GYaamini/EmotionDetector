310404997b8cc22bc3a5ed5dd1f649cd0c426db0
GYaamini <gowriyaamini@gmail.com>
2025-01-16 03:09:55 -0500

    implemented emotion_detector function and packaged EmotionDetection

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    Input = { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(URL, json = Input, headers = Headers)
+    
+    if response.status_code == 200:
+        emotions = json.loads(response.text)["emotionPredictions"][0]["emotion"]
+        dominant_emotion = max(emotions.values())
+        for emotion,score in emotions.items():
+            if score == dominant_emotion:
+                emotions["dominant_emotion"] = emotion
+                break
+
+        return emotions
+    
+    return None
+
+if __name__ == "__main__":
+    text = input("Enter the text input : ")
+    print(emotion_detector(text))
